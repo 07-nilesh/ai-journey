5eb81cf40b364d4c082f1dbecb054e8d1f7b5493
Nilesh <kashyapnilesh0711@gmail.com>
2026-05-09 15:30:44 +0530

    docs : learn how to use json methods

---
+++ phase-1-python/day-19/meet_capture.py
@@ -0,0 +1,11 @@
+import json
+user_settings = '{"theme": "dark", "notifications": true, "capture_limit": 50}'
+data=json.loads(user_settings)
+print(data)
+for key, value in data.items():
+    if key=="capture_limit" and value==50:
+        data[key] = 100
+print(data)
+with open("settings.json", "w") as f:
+    json.dump(data, f, indent=4)
+    print("✅ Settings saved to settings.json")

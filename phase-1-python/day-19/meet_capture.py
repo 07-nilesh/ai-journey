3ac68b4c20ad22d1706752bc4a767a3d5f8bffdb
Nilesh <kashyapnilesh0711@gmail.com>
2026-05-18 11:47:05 +0530

    docs : modified some files

--- phase-1-python/day-19/meet_capture.py
+++ phase-1-python/day-19/meet_capture.py
@@ -5,6 +5,8 @@ print(data)
 for key, value in data.items():
     if key=="capture_limit" and value==50:
         data[key] = 100
+#if "capture_limit" in data:
+#    data["capture_limit"] = 100
 print(data)
 with open("settings.json", "w") as f:
     json.dump(data, f, indent=4)

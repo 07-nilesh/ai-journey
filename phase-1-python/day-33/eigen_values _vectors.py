e553d8c9b19b50e08d8702c22aae4c11e64b95f3
Nilesh <kashyapnilesh0711@gmail.com>
2026-05-28 14:28:35 +0530

    Day 33 | Linear Algebra | Implemented eigenvector decomposition and spectral analysis

---
+++ phase-1-python/day-33/eigen_values _vectors.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+dataset_matrix= np.array([[3,1],[0,2]])
+eigenvalues, eigenvectors = np.linalg.eig(dataset_matrix)
+
+print("Eigenvalues:", eigenvalues)
+print("Eigenvectors:\n", eigenvectors)
+row=eigenvectors[:,0]
+mul=dataset_matrix @ row
+print("Matrix multiplied by the first eigenvector:", mul)
+mul2=eigenvalues[0]*row
+print("First eigenvalue multiplied by the first eigenvector:", mul2)
+
+#variation 1
+import numpy as np
+
+dataset_matrix= np.array([[1,1],[0,1]])
+eigenvalues, eigenvectors = np.linalg.eig(dataset_matrix)
+print("Eigenvalues:", eigenvalues)
+print("Eigenvectors:\n", eigenvectors)

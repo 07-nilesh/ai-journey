a26eea5fd98ae5179d2bbefd84e026cbf04f4733
Nilesh <kashyapnilesh0711@gmail.com>
2026-05-30 00:06:24 +0530

    Day 35 | Stats & Inference | Developed standard error and 95% confidence interval estimator with Bessel's correction

---
+++ phase-1-python/day-34/confidence_interval.py
@@ -0,0 +1,16 @@
+import math
+
+def calculate_CI(data, confidence=0.95):
+    mean = sum(data) / len(data)
+    std_dev = math.sqrt(sum([(x - mean) ** 2 for x in data]) / (len(data) - 1))
+    z_score = 2.58  # For 95% confidence
+    margin_of_error = z_score * (std_dev / math.sqrt(len(data)))
+    lower_bound = mean - margin_of_error
+    upper_bound = mean + margin_of_error
+    return lower_bound, upper_bound, mean
+data=[10,10,10,10]
+sample_data = [10, 12, 14, 18, 20, 22, 24]
+large_sample_data = [10, 12, 14, 18, 20, 22, 24] * 10
+ci_lower, ci_upper, sample_mean = calculate_CI(sample_data)
+print(f"Sample Mean: {sample_mean}")
+print(f"95% Confidence Interval: [{ci_lower:.2f}, {ci_upper:.2f}]")

963b6228cd88570e367510e41cff194bedcd0b28
Nilesh <kashyapnilesh0711@gmail.com>
2026-06-01 00:12:27 +0530

    Day 36 | built custom pip package in editable mode

---
+++ phase-1-python/day-36/my_package_test/my_math/calculator.py
@@ -0,0 +1,3 @@
+def add_numbers(a, b):
+    print("Running the BRAND NEW LIVE code!!!")
+    return a + b
---
+++ phase-1-python/day-36/my_package_test/test_run.py
@@ -0,0 +1,4 @@
+from my_math.calculator import add_numbers
+
+result = add_numbers(5, 10)
+print(f"Result: {result}")

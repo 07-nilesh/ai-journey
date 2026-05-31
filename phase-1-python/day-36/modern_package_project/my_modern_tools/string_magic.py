0a88f83e764e7788d047648d2e10093738507a84
Nilesh <kashyapnilesh0711@gmail.com>
2026-06-01 00:37:38 +0530

    docs : implemented pyproject.toml instead of setup.py for creating packages

---
+++ phase-1-python/day-36/modern_package_project/my_modern_tools/string_magic.py
@@ -0,0 +1,3 @@
+def shout_word(word):
+    """Takes a word and makes it uppercase with excitement!"""
+    return f"{word.upper()}!!!"
---
+++ phase-1-python/day-36/modern_package_project/test_modern.py
@@ -0,0 +1,4 @@
+from my_modern_tools.string_magic import shout_word
+
+result = shout_word("conda is awesome")
+print(result)

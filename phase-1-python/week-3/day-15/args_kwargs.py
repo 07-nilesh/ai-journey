3cc83dfbbf1e2790001f2c4b93990cbfab19442f
Nilesh <kashyapnilesh0711@gmail.com>
2026-07-05 19:48:14 +0530

    Day-15 | function | revised args and kwargs

--- phase-1-python/week-3/day-15/args_kwargs.py
+++ phase-1-python/week-3/day-15/args_kwargs.py
@@ -1,8 +1,14 @@
-def log_workout(user_name, *exercises, **details):
+def log_workout(user_name, duration_minutes, *exercises, **details):
     print(f"User: {user_name}")
+    if len(exercises)!=0:
+        avg=duration_minutes/len(exercises)
+    else:
+        avg=0
+    print(f"Total avg time per exercise :{avg} min")
     
     # args are packed into a tuple
     print(f"Exercises performed: {exercises}")
+
     
     # kwargs are packed into a dictionary
     if "gym_location" in details:
@@ -12,5 +18,6 @@ def log_workout(user_name, *exercises, **details):
         print(f"Meta Info - {key}: {value}")
 
 # Calling the function
-log_workout("Nilesh", "Bench Press", "Squats", "Deadlift", 
-            gym_location="Indore Fitness", duration="45 mins", mood="Energized")
+log_workout("Nilesh",45 , "Bench Press", "Squats", "Deadlift", 
+            gym_location="Indore Fitness", mood="Energized")
+
